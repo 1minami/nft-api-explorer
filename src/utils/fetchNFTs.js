cf711aba4bea96f900b5122b71f31872c3b7c274
1minami <ryo.1122aa@gmail.com>
2022-05-21 17:25:34 +0900

    change eth-mainnet

--- src/utils/fetchNFTs.js
+++ src/utils/fetchNFTs.js
@@ -1,7 +1,7 @@
 // Go to www.alchemy.com and create an account to grab your own api key!
 const { alchemyApiKey } = require("../secrets.json");
 const apiKey = alchemyApiKey;
-const endpoint = `https://eth-rinkeby.alchemyapi.io/v2/${apiKey}`;
+const endpoint = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`;
 
 export const fetchNFTs = async (owner, contractAddress, setNFTs, retryAttempt) => {
     if (retryAttempt === 5) {
